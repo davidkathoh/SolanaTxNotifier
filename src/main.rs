a6ed1b1ff6612dd801f32358ffccbf6641f97322
David Kathoh <mudavidkat@gmail.com>
2024-05-06 23:14:22 +0200

    added teloxide for telegram integration

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use actix_web::{get,post,web,App,HttpResponse,HttpServer,Responder};
+use actix_web::{get,post,web,App,HttpResponse,HttpServer,Responder, Result};
+use core::sync;
 use std::env;
 use  dotenv::dotenv;
+use teloxide::prelude::*;
 
 
 #[get("/")]
 async fn hello() -> impl Responder{
-    let api_key = env::var("API_KEY").expect("You haven't set your api key");
-    HttpResponse::Ok().body(api_key)
+
+
+    HttpResponse::Ok().body("Hello world")
 }
 
 #[post("/rpc")]
@@ -14,13 +17,17 @@ async fn rpc_webhook(req_body:String) -> impl Responder{
     HttpResponse::Ok().body(req_body)
 }
 #[post("/telegram")]
-async fn telegram_webhook(req_body:String) -> impl Responder{
-    HttpResponse::Ok().body(req_body)
+async fn telegram_webhook(update:teloxide::types::Update,) -> Result<HttpResponse,Box<dyn std::error::Error + Send + sync>>{
+    println!("Received update: {:?}",update);
+    Ok(HttpResponse::Ok().finish())
 }
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
     dotenv().ok();
+    pretty_env_logger::init();
+    let bot = Bot::from_env();
+
     HttpServer::new(||{
         App::new()
         .service(hello)
