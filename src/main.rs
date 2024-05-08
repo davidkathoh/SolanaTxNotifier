d4915a978c8f99849f58d33f25f10dd4d9b0e2ec
David Kathoh <mudavidkat@gmail.com>
2024-05-08 14:42:41 +0200

    testing shared mutable data

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,9 @@ async fn hello(bot:web::Data<Bot>, account:web::Data<TrackedAddress>) -> impl Re
 
     let mut acc_ = account.account.lock().unwrap();
      if let Some(chat_ids) = acc_.get_mut("0x") {
+        
         println!("John's numbers: {:?}", chat_ids);
-        let _ = bot.send_message(ChatId::from(user_id),chat_ids).await;
+        let _ = bot.send_message(ChatId::from(user_id),chat_ids.iter().map(|&id| id.to_string()).collect::<Vec<String>>().join(",")).await;
 
         let rnd_id:u64 = rand::thread_rng().gen();
         chat_ids.push(rnd_id);
