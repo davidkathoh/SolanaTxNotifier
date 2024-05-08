2ab9256af0eee2880d04b509f5bafd7fb903ea6f
David Kathoh <mudavidkat@gmail.com>
2024-05-08 14:28:48 +0200

    testing shared mutable data

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, ResponseError, Result};
-use std::env;
+use std::{collections::HashMap, env};
 use  dotenv::dotenv;
 use teloxide::{ prelude::*,};
 use serde_json::Value;
+use std::sync::Mutex;
+use rand::Rng;
 
 
 #[get("/")]
-async fn hello(bot:web::Data<Bot>) -> impl Responder{
+async fn hello(bot:web::Data<Bot>, account:web::Data<TrackedAddress>) -> impl Responder{
 
     let user_id = UserId(5331817989);
     let _ = bot.send_message(ChatId::from(user_id),"Hello world").await;
+
+    let mut acc_ = account.account.lock().unwrap();
+     if let Some(chat_ids) = acc_.get_mut("0x") {
+        println!("John's numbers: {:?}", chat_ids);
+        let rnd_id:u64 = rand::thread_rng().gen();
+        chat_ids.push(rnd_id);
+    }else{
+    acc_.insert("0x".to_string(), vec![34,56]);
+    }
+    
     HttpResponse::Ok().body("Hello world")
 }
 
@@ -40,16 +52,21 @@ async fn telegram_webhook(body: web::Json<Update>,bot:web::Data<Bot>) -> impl Re
     println!("message sent");
     HttpResponse::Ok()
 }
+
+struct TrackedAddress{
+    account:Mutex<HashMap<String,Vec<u64>>>,
+}
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
     dotenv().ok();
     pretty_env_logger::init();
     let bot = Bot::from_env();
-
+    let account = web::Data::new(TrackedAddress{account:Mutex::new(HashMap::new())});
     HttpServer::new(move||{
         App::new()
         .app_data(web::Data::new(bot.clone()))
+        .app_data(account.clone())
         .service(hello)
         .service(rpc_webhook)
         .service(telegram_webhook)
